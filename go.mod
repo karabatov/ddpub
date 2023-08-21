09c84f6cc8637dd0c89c9d5894ab6112fce75561
Yuri Karabatov <yuri@yurikarabatov.com>
2023-08-21 22:25:04 +0100

    Try loading a TOML

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/karabatov/ddpub
 
-go 1.20
+go 1.21
+
+require github.com/pelletier/go-toml/v2 v2.0.9 // indirect
--- main.go
+++ main.go
@@ -3,11 +3,28 @@ package main
 import (
 	"flag"
 	"fmt"
+
+	"github.com/pelletier/go-toml/v2"
 )
+
+type DDConfig struct {
+	Address string
+}
 
 func main() {
 	var notesDir = flag.String("notesDir", ".", "Directory that stores notes")
 	flag.Parse()
 	fmt.Println(*notesDir)
 	flag.Usage()
+
+	static := `
+	address = "ddpub.org"
+	`
+
+	var cfg DDConfig
+	err := toml.Unmarshal([]byte(static), &cfg)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("address:", cfg.Address)
 }
