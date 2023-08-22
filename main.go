c5ae23dc52282a8cfd19393bd5ae987ebdeb250c
Yuri Karabatov <yuri@yurikarabatov.com>
2023-08-23 00:32:33 +0100

    Try to open the config file

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
+	"path/filepath"
 
 	"github.com/pelletier/go-toml/v2"
 )
@@ -13,19 +15,41 @@ type DDConfig struct {
 }
 
 func main() {
-	var notesDir = flag.String("notesDir", ".", "Directory that stores notes")
-	flag.Parse()
-	fmt.Println(*notesDir)
-	flag.Usage()
+	argsLen := len(os.Args[1:])
+	// At least the command must be present.
+	if argsLen == 0 {
+		fmt.Println("Command is missing. Example:")
+		fmt.Println("    ddpub check --config <dir> --notes <dir>")
+		os.Exit(1)
+	}
+	command := os.Args[1]
+	if command != "check" {
+		fmt.Println("Only check command is supported")
+		fmt.Println("    ddpub check --config <dir> --notes <dir>")
+		os.Exit(1)
+	}
 
-	static := `
-	address = "ddpub.org"
-	`
+	// Maybe refactor to `FlagSet` later, per command.
+
+	var configDir = flag.String("config", ".", "Directory that has `config.toml`")
+	var notesDir = flag.String("notes", ".", "Directory that stores notes")
+	flag.Parse()
+	fmt.Println(*configDir, *notesDir)
+
+	// Try to read the config file.
+	*configDir = filepath.Clean(*configDir)
+	configPath := filepath.Join(*configDir, "config.toml")
+	configFile, err := os.ReadFile(configPath)
+	if err != nil {
+		fmt.Printf("Could not open config file '%s':\n\t%v", configPath, err)
+		os.Exit(1)
+	}
 
 	var cfg DDConfig
-	err := toml.Unmarshal([]byte(static), &cfg)
+	err = toml.Unmarshal(configFile, &cfg)
 	if err != nil {
-		panic(err)
+		fmt.Printf("Could not read config file '%s':\n\t%v", configPath, err)
+		os.Exit(1)
 	}
 	fmt.Println("address:", cfg.Address)
 }
