99733a556e127f6e7a0e00ae6664031c6a2f21fa
Yuri Karabatov <yuri@yurikarabatov.com>
2023-09-16 00:03:30 +0100

    Add Website struct

--- config/config.go
+++ config/config.go
@@ -1,4 +1,12 @@
 // Package config loads and validates website config from a directory.
 package config
 
+import "fmt"
 
+// Website represents the configuration of a website.
+type Website struct {
+}
+
+func Load(configDir string) (Website, error) {
+	return Website{}, fmt.Errorf("not implemented")
+}
