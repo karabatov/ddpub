b99178cfa364b33402c0109ed577bb3e43a2f606
Yuri Karabatov <yuri@yurikarabatov.com>
2023-09-20 20:14:55 +0100

    Rewrite to use FlagSet

--- main.go
+++ main.go
@@ -10,31 +10,63 @@ import (
 	"github.com/karabatov/ddpub/notes"
 )
 
+const (
+	usage = `ddpub is a tool to serve one set of notes as many websites.
+
+	Check config:
+			ddpub check --config <dir> --notes <dir>
+
+	Serve notes:
+			ddpub serve --config <dir> --notes <dir> --port <port>`
+)
+
+var (
+	configDir string
+	notesDir  string
+	port      int
+)
+
 func main() {
 	startTime := time.Now()
-	fmt.Println(os.Args)
 
-	// Maybe refactor to `FlagSet` later, per command.
-
-	checkCmd := flag.Bool("check", false, "Check the config")
-	serveCmd := flag.Bool("serve", false, "Serve the notes")
-	configDir := flag.String("config", ".", "Directory that has `config.toml`")
-	notesDir := flag.String("notes", ".", "Directory that stores notes")
-	flag.Parse()
-
-	if !(*checkCmd || *serveCmd) {
-		fmt.Println("Command is missing. Example:")
-		fmt.Println("    ddpub --check --config <dir> --notes <dir>")
+	if len(os.Args) < 3 {
+		fmt.Println(usage)
 		os.Exit(1)
 	}
 
-	cfg, err := config.Load(*configDir)
+	const (
+		configFlag  = "config"
+		configUsage = "Directory that has `config.toml`"
+		notesFlag   = "notes"
+		notesUsage  = "Directory that stores notes"
+	)
+
+	switch os.Args[1] {
+	case "check":
+		check := flag.NewFlagSet("check", flag.ExitOnError)
+		check.Usage = func() { fmt.Println(usage) }
+		check.StringVar(&configDir, configFlag, ".", configUsage)
+		check.StringVar(&notesDir, notesFlag, ".", notesUsage)
+		check.Parse(os.Args[2:])
+	case "serve":
+		serve := flag.NewFlagSet("serve", flag.ExitOnError)
+		serve.Usage = func() { fmt.Println(usage) }
+		serve.StringVar(&configDir, configFlag, ".", configUsage)
+		serve.StringVar(&notesDir, notesFlag, ".", notesUsage)
+		serve.IntVar(&port, "port", 44234, "Port to serve notes")
+		serve.Parse(os.Args[2:])
+	default:
+		fmt.Println(usage)
+		os.Exit(1)
+	}
+
+	cfg, err := config.Load(configDir)
 	if err != nil {
 		fmt.Printf("Couldn't load website config: %v\n", err)
 		os.Exit(1)
 	}
 
-	store, err := notes.Load(cfg, *notesDir)
+	store, err := notes.Load(cfg, notesDir)
 	if err != nil {
 		fmt.Printf("Couldn't load notes: %v\n", err)
 		os.Exit(1)
@@ -44,10 +76,11 @@ func main() {
 	// At this point the surface check is complete! There may be more
 	// errors like duplicate tags or bad URLs, but these will be caught later.
 	fmt.Printf("Config OK. Startup took %v.", time.Since(startTime))
-	if *checkCmd && !*serveCmd {
+
+	// If we were only checking the config, exit now.
+	if os.Args[1] == "check" {
 		os.Exit(0)
 	}
-	if !*serveCmd {
-		os.Exit(1)
-	}
+
+	// Serve notes.
 }
