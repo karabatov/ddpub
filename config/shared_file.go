a576b62830ea419cc321a443a191195c4daf1f9c
Yuri Karabatov <yuri@yurikarabatov.com>
2023-11-29 18:33:44 +0000

    Add SharedFile.overload

--- config/shared_file.go
+++ config/shared_file.go
@@ -1,5 +1,11 @@
 package config
 
+import (
+	"net/http"
+	"os"
+	"path/filepath"
+)
+
 // SharedFile is a file common to the whole website, embedded by default
 // but can be overloaded if a certain file is present in the config dir.
 type SharedFile struct {
@@ -9,4 +15,17 @@ type SharedFile struct {
 	Content []byte
 	// ContentType goes into the HTTP header.
 	ContentType string
+}
+
+// overload tries to read the file from configDir and replaces Content and ContentType.
+func (sf *SharedFile) overload(configDir string) {
+	path := filepath.Join(configDir, sf.Filename)
+	f, err := os.ReadFile(path)
+	if err != nil {
+		// Don't overload if we get an error.
+		return
+	}
+
+	sf.Content = f
+	sf.ContentType = http.DetectContentType(f)
 }
